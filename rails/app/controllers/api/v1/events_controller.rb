3da8e298d2c1325c06a034bbab8aee724e899a53
Thomas DeMeo <thomasdemeo@gmail.com>
2013-06-18 04:12:33 -0400

    Added Events Service for retuning all events for a valid department id;

--- rails/app/controllers/api/v1/events_controller.rb
+++ rails/app/controllers/api/v1/events_controller.rb
@@ -5,7 +5,17 @@ module Api
       respond_to :json
       
       def index
-        respond_with Event.all
+        department = params[:department]
+        # Add this back in
+        # valid_dep = Department.find_by_valid_key_and_customer_id(department[:valid_key], department[:customer_id])
+        valid_dep = Department.validate_key(params[:valid_key])
+        
+        # events = Event.find_by_department_id_updated_after(valid_dep[:id], params[:updated_at])
+        events = Event.where("department_id = ?", valid_dep[:id])
+        # events = Event.find_by_department_id(valid_dep[:id])
+        Rails.logger.info(events)
+        
+        respond_with events
       end
       
       def show
@@ -24,10 +34,13 @@ module Api
         respond_with Event.destroy(params[:id])
       end
       
-      private
+      # private
       def check_department_key
         department = params[:department]
-        valid_dep = Department.find_by_valid_key_and_customer_id(department[:valid_key], department[:customer_id])
+        # Add this back in
+        # valid_dep = Department.find_by_valid_key_and_customer_id(department[:valid_key], department[:customer_id])
+        valid_dep = Department.validate_key(params[:valid_key])
+        
         head :unauthorized unless valid_dep
       end
       
