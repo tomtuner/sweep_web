50a60ec3fa20eb86f66e50a907a80034050ccd90
Thomas DeMeo <thomasdemeo@gmail.com>
2013-08-30 13:12:36 -0400

    Added correct url link to Admin page if an administrator;

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -5,6 +5,7 @@ Sweep::Application.routes.draw do
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
+  
   # get '/department_validation#index', to: 'department_validation#create'
 
 	namespace :api, :defaults => {:format => 'json'} do
