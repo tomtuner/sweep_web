d2a8ef816b6b0fb346359736134469d343117bf2
Thomas DeMeo <thomasdemeo@gmail.com>
2014-01-08 16:18:34 -0500

    Added total for month to dash; Dash needs more work;

--- rails/app/controllers/dashboard_controller.rb
+++ rails/app/controllers/dashboard_controller.rb
@@ -1,6 +1,16 @@
 class DashboardController < ApplicationController
   def index
-    @scans = Scan.where(:event_id => "2").all
+    # @scans = Scan.where(:event_id => "2").all
     
+    @beg_of_month = Date.today - (Time.now.day) + 1
+    @month_scans = Scan.where("created_at BETWEEN ? AND ?", @beg_of_month, Time.now)
+    
+    @advisors = Advisor.find_all_by_user_id(current_user[:id])
+    @events = Event.where("department_id IN (?)", @advisors.map(&:department_id))
+    @month_scans = Scan.where("event_id IN (?) AND created_at BETWEEN ? AND ?", @events.map(&:id), @beg_of_month, Time.now)
+    
+    # @month_scans = Scan.joins("INNER JOIN events ON scans.event_id = events.id").joins("INNER JOIN departments ON events.department_id IN (?)", @advisors.map(&:department_id))
+#     @month_scans = 
+#     @scans = Scan.where("event_id IN (?)", @events.map(&:id))
   end
 end
