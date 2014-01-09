ef3528d5a196a26ca8f4d1cfa4257047e347383f
Thomas DeMeo <thomasdemeo@gmail.com>
2014-01-09 16:11:18 -0500

    Added two computations for month scans and month unique students; Began work on arrow indicating increase or decrease since last month;

--- rails/app/controllers/dashboard_controller.rb
+++ rails/app/controllers/dashboard_controller.rb
@@ -9,6 +9,9 @@ class DashboardController < ApplicationController
     @events = Event.where("department_id IN (?)", @advisors.map(&:department_id))
     @month_scans = Scan.where("event_id IN (?) AND created_at BETWEEN ? AND ?", @events.map(&:id), @beg_of_month, Time.now)
     
+    # @month_scans_uniq = 0
+    @month_scans_uniq = Scan.where("event_id IN (?) AND CREATED_at BETWEEN ? AND ?", @events.map(&:id), @beg_of_month, Time.now).count('value', :distinct => true)
+    
     # @month_scans = Scan.joins("INNER JOIN events ON scans.event_id = events.id").joins("INNER JOIN departments ON events.department_id IN (?)", @advisors.map(&:department_id))
 #     @month_scans = 
 #     @scans = Scan.where("event_id IN (?)", @events.map(&:id))
