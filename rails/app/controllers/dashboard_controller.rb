28c4b39ec1270d018d816d5fa2bcbf9d6b527318
Thomas DeMeo <thomasdemeo@gmail.com>
2014-03-10 11:56:45 -0400

    RC1 for pagination release;

--- rails/app/controllers/dashboard_controller.rb
+++ rails/app/controllers/dashboard_controller.rb
@@ -3,11 +3,11 @@ class DashboardController < ApplicationController
     # @scans = Scan.where(:event_id => "2").all
     
     @beg_of_month = Date.today - (Time.now.day) + 1
-    @user = current_user
-    @department = current_department
-    if @user
+    
+    if @user = current_user
       @advisors = Advisor.find_all_by_user_id(current_user[:id])
-      @events = Event.where("department_id IN (?)", @advisors.map(&:department_id)).order("starts_at DESC").page(params[:page]).per(20)
+      @month_events = Event.select("id").where("department_id IN (?) AND starts_at BETWEEN ? and ?", @advisors.map(&:department_id), @beg_of_month, Time.now)
+      @events = Event.select("id, name, starts_at").where("department_id IN (?)", @advisors.map(&:department_id)).order("starts_at DESC").page(params[:page]).per(15)
       if @advisors.count > 1
         @departments = Department.find(@advisors.map(&:department_id).uniq)
       elsif @advisors.count == 1
@@ -17,13 +17,14 @@ class DashboardController < ApplicationController
       
       end
       
-    elsif @department
-      @events = Event.where("department_id IN (?)", @department.id).order("starts_at DESC").page(params[:page]).per(20)
+    elsif @department = current_department
+      @month_events = Event.select("id").where("department_id IN (?) AND starts_at BETWEEN ? AND ?", @department.id, @beg_of_month, Time.now)
+      @events = Event.select("id, name, starts_at").where("department_id IN (?)", @department.id).order("starts_at DESC").page(params[:page]).per(15)
       @scans = Scan.select("event_id, value, created_at").where("event_id IN (?)", @events.map(&:id))
     end
     
-    @month_scans = Scan.where("event_id IN (?) AND created_at BETWEEN ? AND ?", @events.map(&:id), @beg_of_month, Time.now).count
-    @month_scans_uniq = Scan.where("event_id IN (?) AND CREATED_at BETWEEN ? AND ?", @events.map(&:id), @beg_of_month, Time.now).count('value', :distinct => true)
+    @month_scans = Scan.where("event_id IN (?)", @month_events.map(&:id)).count
+    @month_scans_uniq = Scan.where("event_id IN (?)", @month_events.map(&:id)).count('value', :distinct => true)
 
     
 
